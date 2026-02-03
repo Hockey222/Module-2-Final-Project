91979aae6108e0e85c50288910e627aebe563235
Hockey222 <BradleyG43@gmail.com>
2026-02-03 16:39:43 -0500

    stuck on the nav

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function openMenu () {
 document.body.classList.add('menu--open');
 }
 function closeMenu() {
-    document.body.classList.remove(menu--open);
+    document.body.classList.remove('menu--open');
 }
